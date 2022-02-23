1c74a12f8a753557bf5560f4db95d6f06a886bb3
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-02-23 16:12:12 +0100

    i numeri fino a 100

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let boxNumero = document.querySelector('.carte');
+
+
+
+for (let numero = 0; numero <= 100; numero++) {
+
+    boxNumero.innerHTML = boxNumero.innerHTML + `<div class="box">${numero}<\div>`
+
+}
+
+
+
