0b8aad164bc7f4b9d69d57900b160c2ef9027bd8
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-02-23 16:55:38 +0100

    x3 - x5 - x15 colorati

--- js/script.js
+++ js/script.js
@@ -2,11 +2,24 @@ let boxNumero = document.querySelector('.carte');
 
 
 
-for (let numero = 0; numero <= 100; numero++) {
+for (let numero = 1; numero <= 100; numero++) {
 
-    boxNumero.innerHTML = boxNumero.innerHTML + `<div class="box">${numero}<\div>`
+
+    if (numero % 3 == 0 && numero % 5 == 0) {
+        boxNumero.innerHTML = boxNumero.innerHTML + `<div class="box fizzbuzz">fizzbuzz<\div>`
+
+    } else if (numero % 5 == 0) {
+        boxNumero.innerHTML = boxNumero.innerHTML + `<div class="box buzz">buzz<\div>`
+
+    } else if (numero % 3 == 0) {
+        boxNumero.innerHTML = boxNumero.innerHTML + `<div class="box fizz">fizz<\div>`
+
+    } else {
+        boxNumero.innerHTML = boxNumero.innerHTML + `<div class="box">${numero}<\div>`
+
+    }
+
 
 }
-
 
 
